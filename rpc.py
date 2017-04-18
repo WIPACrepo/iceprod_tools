ce9455891451d9d166d78c129bff07e4d1865eec
David Delventhal <ddelventhal@icecube.wisc.edu>
2017-04-18 11:34:58 -0500

    Added more methods.

--- rpc.py
+++ rpc.py
@@ -3,7 +3,7 @@ class RPC:
     def __init__(self, server_address):
         self.id = 0
         self.server_address = server_address
-    def __call__(self, method, params=None):
+    def __call__(self, method, **params):
         self.id += 1
         data = {'jsonrpc':'2.0','method':'public_' + method,'id':self.id}
         if params != None: data['params'] = params
@@ -13,13 +13,16 @@ class RPC:
     def get_number_of_tasks_in_each_state(self):
         return self('get_number_of_tasks_in_each_state')
     def get_datasets_by_status(self, status):
-        return self('get_datasets_by_status', {'status': status})
+        return self('get_datasets_by_status', status=status)
     def get_config(self, dataset_id):
-        return self('get_config', {'dataset_id': dataset_id})
-
-    def get_task_runtime(self, task_id):
-        pass
+        return self('get_config', dataset_id=dataset_id)
+    def get_task_walltime(self, task_id):
+        return self('get_task_walltime', task_id=task_id)
     def get_task_ids(self, dataset_id):
-        pass
+        return self('get_task_ids', dataset_id=dataset_id)
     def get_task_stats(self, task_id):
-        pass
+        return self('get_task_stats', task_id=task_id)
+    def get_dataset_description(self, dataset_id):
+        return self('get_dataset_description', dataset_id=dataset_id)
+    def get_dataset_steering(self, dataset_id):
+        return self('get_dataset_steering', dataset_id=dataset_id)
